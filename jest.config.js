f074538b8a2fdd77ff54554cf1331e43009e1507
nobujose <nobujose6@gmail.com>
2025-10-31 01:06:43 -0400

    feat: configuracion pruebas y Calidad y Formateo de Código

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,9 @@
-const nextJest = require('next/jest')();
-
-const createJestConfig = nextJest({
-  dir: './',
-});
-
-/** @type {import('jest').Config} */
-const config = {
-  testEnvironment: 'jest-environment-jsdom',
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    // Maneja el alias de tu jsconfig.json
     '^@/(.*)$': '<rootDir>/src/$1',
   },
 };
-
-module.exports = createJestConfig(config);
